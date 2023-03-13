9a30d64c11a47316eb61d09a537d2976e8f3b2b3
Batuhan Gülcemal <bthnglcml001@hotmail.com>
2023-03-13 10:31:42 +0300

    FreeLookCamera pre-

--- Assets/Scripts/LocalScripts/PlayerController.cs
+++ Assets/Scripts/LocalScripts/PlayerController.cs
@@ -1,10 +1,14 @@
+using Cinemachine;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance { get; private set; }
 
     [SerializeField] public Player player;
+    [SerializeField] public CinemachineFreeLook CMcamera;
+    [SerializeField] public Transform HeadTransform;
 
     private void Awake()
     {
@@ -16,7 +20,11 @@ public class PlayerController : MonoBehaviour
         player = _player;
         transform.SetParent(player.transform, false);
 
+        CMcamera.Follow = HeadTransform;
+        CMcamera.LookAt = HeadTransform;
+
         GetComponent<PlayerAnimation>().enabled = true;
+
     }
 
     public void DetachPlayer()
